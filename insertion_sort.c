84312784351a14761f5f5aa9f54e5b420f3afbd1
raul-leal <108129342+raul-leal@users.noreply.github.com>
2024-04-23 23:12:28 +0000

    10/10 no moodle

--- insertion_sort.c
+++ insertion_sort.c
@@ -12,3 +12,66 @@ void printArray(Element *array, int size) {
     }
     printf("]\n");
 }
+
+void insertionSort(Element* array, int size, bool withTrace){
+    int cur = 1;
+    while(cur < size){
+        int temp = array[cur];
+        for(int i=cur; i>0; i--){
+            if(i-1 == 0 && array[i-1] > temp){
+                array[i] = array[i-1];
+                array[i-1] = temp;
+                if(withTrace)
+                    printArray(array, size);
+                break;
+            }else if(array[i-1]>temp){
+                array[i] = array[i-1];
+            } else if(array[i-1] <= temp){
+                array[i] = temp;
+                if(withTrace)
+                    printArray(array, size);
+                break;
+            }
+        }
+    cur++;
+    }
+}
+
+/* NÃO COPIAR ESSA PARTE
+
+int findPartition(Element* array, int size){
+    int i = 1;
+    for(i; i<size-1; i++){
+        if(array[i] < array[0]){
+            break;
+        }
+    }
+    return i;
+}
+
+void insertionSort(Element *array, int size, bool withTrace){
+    //Primeiro elemento da partição não ordenada
+    int first = findPartition(array, size);
+    while(first < size){
+        int temp = array[first];
+        for(int i = first-1; i>=0; i--){
+            if(array[i] < temp){
+                array[i+1] = temp;
+                if(withTrace)
+                    printArray(array, size);
+                break;
+            }
+            else if(i == 0){
+                array[i+1] = array[i];
+                array[i] = temp;
+                if(withTrace)
+                    printArray(array, size);
+                break;
+            }
+            else if(array[i] > temp){
+                array[i+1] = array[i];
+            }
+        }
+        first++;
+}
+*/
